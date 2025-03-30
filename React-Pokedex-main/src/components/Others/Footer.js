79a3060e8e2605f247056d231edbf108bfe8e37d
sabrinacsilva <sabrina.c.silva@outlook.com>
2025-03-30 16:11:08 -0300

    Primeiro commit

---
+++ React-Pokedex-main/src/components/Others/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./styles.css";
+
+const Footer = () => {
+  return (
+    <footer className="mt-5">
+      <div className="d-flex justify-content-center align-items-center">
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
